01412ee3d5ad167deeff66325b6cfc71d5b4b352
bjigeon <bjigeon@gmail.com>
2023-04-07 08:58:38 +0900

    change imgName & create button css

--- src/components/result/Result.style.js
+++ src/components/result/Result.style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
-// import lufftBtn from "../../assets/result/lufftBtn.svg";
-// import kuromiBtn from "../../assets/result/kuromiBtn.svg";
-// import ryanBtn from "../../assets/result/ryanBtn.svg";
+import lufftBtn from "../../assets/result/luffyBtn.svg";
+import kuromiBtn from "../../assets/result/kuromiBtn.svg";
+import ryanBtn from "../../assets/result/ryanBtn.svg";
 
 export const Result = styled.div`
   width: 100%;
@@ -51,7 +51,8 @@ export const Setting = styled.div`
       button {
         width: 114px;
         height: 114px;
-        background: #d9d9d9;
+        /* background: #d9d9d9; */
+        /* background-color: rgb(236, 236, 236); */
         border-radius: 15px;
         border: none;
 
@@ -59,9 +60,6 @@ export const Setting = styled.div`
         font-weight: 700;
         font-size: 36px;
         color: #000000;
-      }
-
-      button[id="selected"] {
       }
     }
   }
@@ -113,18 +111,23 @@ export const Setting = styled.div`
         background: white;
       }
 
-      /* button[id="luffy"] {
+      button[id="luffy"] {
+        background-color: white;
         background-image: url(${lufftBtn});
-      } */
-      /* 
+      }
+
       button[id="kuromi"] {
         background: url(${kuromiBtn});
       }
 
       button[id="ryan"] {
         background: url(${ryanBtn});
-      } */
+      }
     }
+  }
+
+  .selected {
+    background-color: rgb(235, 195, 194);
   }
 `;
 
