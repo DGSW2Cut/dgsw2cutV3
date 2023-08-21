9f46fce8cbbda76de814cd5b9c285d600e53b363
bae0203 <gmbae06@dgsw.hs.kr>
2023-08-21 21:34:03 +0900

    Fix :: Style

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -30,101 +30,86 @@ const App = () => {
   };
 
   return (
-    <Container>
-      <img src={cam1} alt="카메라 사진" id="cam" />
+    <MainContainer>
       <img
-        ref={imageRef}
-        src=""
-        alt="hiddenImage"
-        style={{ visibility: "hidden", position: "absolute" }}
+        src={logo}
+        alt="학교 로고"
+        style={{ width: "10rem", position: "fixed", bottom: 50, left: 40 }}
       />
-      <div id="canvase">
-        <canvas ref={canvasRef} style={{ width: 800, height: 600 }} />
-      </div>
-      <Info>
-        <img src={logo} alt="학교 로고" />
-        <ButtonContainer>
-          <Btn onClick={captureImg}>촬영</Btn>
-          <Cnt>{imageList.length} / 2</Cnt>
-        </ButtonContainer>
-      </Info>
-    </Container>
+      <ImageContainer>
+        <div style={{ width: "44%" }}>
+          <img
+            ref={imageRef}
+            src=""
+            alt="hiddenImage"
+            style={{ visibility: "hidden", position: "absolute" }}
+          />
+          <div id="canvase">
+            <canvas
+              ref={canvasRef}
+              style={{ width: "100%", borderRadius: "10px" }}
+            />
+          </div>
+          <p style={{ textAlign: "center" }}>
+            <p>표정에 따라 배경이 바뀌어요</p>
+            <CaptureButton onClick={captureImg}>촬영</CaptureButton>
+          </p>
+        </div>
+        <PreviewImageContainer>
+          <p>사진 미리보기</p>
+          <PreviewImage image={imageList[0]} />
+          <PreviewImage image={imageList[1]} />
+        </PreviewImageContainer>
+      </ImageContainer>
+    </MainContainer>
   );
 };
 
 export default App;
 
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly;
-
-  width: 1920px;
-  height: 1080px;
-
-  canvas {
-    border-radius: 30px;
-  }
-
-  #cam {
-    z-index: 1;
-    position: relative;
-  }
-
-  #canvase {
-    z-index: 2;
-    position: absolute;
-    top: 360px;
-    left: 150px;
-  }
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100vh;
 `;
 
-const Info = styled.div`
+const PreviewImage = styled.div`
+  width: 20rem;
+  margin: 1rem;
+  height: 15rem;
+  background: ${(props) => (props.image ? `url(${props.image})` : "white")};
+  background-size: cover;
+  border: ${(props) => (props.image ? "none" : "1px solid #A8A8A8")};
+  border-radius: 10px;
+`;
+const PreviewImageContainer = styled.div`
+  width: 20%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
+  padding-bottom: 5rem;
+`;
+
+const ImageContainer = styled.div`
+  width: 100%;
+  display: flex;
   justify-content: space-evenly;
+  padding-top: 6rem;
 `;
 
-const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 60vh;
-  gap: 30px;
-`;
-const Btn = styled.button`
-  width: 232px;
-  height: 166px;
-  background-color: #f6cad6;
-  /* opacity: 0.5; */
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 15px;
-  border: none;
-  color: #000000;
-
-  font-style: normal;
-  font-weight: 700;
-  font-size: 36px;
-
-  cursor: pointer;
-`;
-
-const Cnt = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 232px;
-  height: 166px;
-
-  background: #f6cad6;
-  /* opacity: 0.5; */
-  border-radius: 15px;
-
-  font-style: normal;
-  font-weight: 700;
-  font-size: 36px;
-  line-height: 44px;
-  color: #000000;
+const CaptureButton = styled.button`
+  box-shadow: none;
+  margin: 1rem auto;
+  padding: 2.25rem 3.25rem;
+  border-radius: 10px;
+  border: 1px solid #a8a8a8;
+  font-size: 2rem;
+  font-weight: 600;
+  background-color: white;
+  &:hover {
+    background-color: #ffe2ea;
+  }
+  &:active {
+    background-color: #f6cad6;
+  }
 `;
