4840ffb9d7bdb10005f4333f8bd549cea13d716d
bae0203 <gmbae06@dgsw.hs.kr>
2023-08-21 21:33:01 +0900

    ADD :: Doguri Version

--- src/components/result/Adornment.jsx
+++ src/components/result/Adornment.jsx
@@ -1,32 +1,34 @@
 import React, { useMemo } from "react";
-import { useRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
 import { pictureState } from "../../atom/picture";
 
 import ryan from "../../assets/result/back/choonsikBack.png";
 import kuromi from "../../assets/result/back/kuromiBack.png";
 import luffy from "../../assets/result/back/luffyBack.png";
 import luffyChar from "../../assets/result/back/luffyChar.png";
+import doguri from "../../assets/result/back/doguriBack.png";
+import doguriChar from "../../assets/result/back/doguriChar.png";
 import kuromiChar from "../../assets/result/back/kuromiChar.png";
 import ryanChar from "../../assets/result/back/choonsikChar.png";
-import coex from "../../assets/result/back/coex.png";
-import coexBlack from "../../assets/result/back/coexBlack.png";
+import coex from "../../assets/result/back/Normal.png";
+import coexBlack from "../../assets/result/back/Black.png";
 
 import * as A from "./Adornment.style";
 
 /**
- * @param {{cnt: number, back?: "luffy" | "kuromi" | "ryan" | "black" | "white"}}
+ * @param {{cnt: number, back?: "luffy" | "kuromi" | "ryan" | "doguri" | "black" | "white"}}
  * @returns {JSX.Element}
  */
 const Adornment = ({ cnt, back }) => {
-  const [imageList] = useRecoilState(pictureState);
+  const imageList = useRecoilValue(pictureState);
   const BackgroundImg = useMemo(
-    () => ({ ryan, kuromi, luffy, black: coexBlack, white: coex }),
+    () => ({ ryan, kuromi, luffy, doguri, black: coexBlack, white: coex }),
     []
   );
 
   return (
     <A.Container>
-      {new Array(2).fill(0).map((_, idx) => (
+      {new Array(cnt).fill(0).map((_, idx) => (
         <A.ImgContainer key={idx} id={idx}>
           <CharImage back={back} />
           <A.TemplateImg src={BackgroundImg[back] || ryan} alt="template" />
@@ -35,7 +37,6 @@ const Adornment = ({ cnt, back }) => {
           ))}
         </A.ImgContainer>
       ))}
-      {cnt === 4 && <p>X 2</p>}
     </A.Container>
   );
 };
@@ -62,7 +63,10 @@ const CharImage = ({ back }) => {
     case "ryan":
       img = ryanChar;
       break;
+    case "doguri":
+      img = doguriChar;
+      break;
   }
 
-  return <>{<A.TemplateImg id={back} src={img} alt="" />}</>;
+  return <>{<A.TemplateCharImg id={back} src={img} alt="" />}</>;
 };
